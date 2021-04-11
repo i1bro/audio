43bdb2ea03cf30125430a5ee4577fe17911cea88
Ilya <ilya.brovarnik@gmail.com>
2021-04-11 19:35:42 +0300

    add commands

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,7 @@ import java.io.File
 import java.lang.Exception
 import java.lang.IllegalArgumentException
 
-open class Node(var value: String, var nodes: List<Node> = listOf())
+open class Node(var value: String, var nodes: MutableList<Node> = mutableListOf())
 
 fun loadFromFile(fileName: String): Node? {
     return try {
@@ -19,7 +19,7 @@ fun loadFromFile(fileName: String): Node? {
             for (j in numbers) {
                 currentNodes.add(loadedNodes[j])
             }
-            loadedNodes[i].nodes = currentNodes.toList()
+            loadedNodes[i].nodes = currentNodes
         }
         reader.close()
 
@@ -70,21 +70,65 @@ fun saveToFile(fileName: String, root: Node?) {
 }
 
 class NodeOperator {
-    val SavedNodes: ArrayList<Node> = arrayListOf();
+    private val savedNodes: ArrayList<Node> = arrayListOf()
+
+    private fun checkIndex(nodeInd: Int) {
+        if(nodeInd >= savedNodes.size || nodeInd < 0) {
+            throw IllegalArgumentException("Index out of range")
+        }
+    }
+
+    private fun checkIndex(nodeInd: Int, childInd: Int) {
+        checkIndex(nodeInd)
+        if(childInd >= savedNodes[nodeInd].nodes.size || childInd < 0) {
+            throw IllegalArgumentException("Index out of range")
+        }
+    }
 
     fun addIsolatedNode(value: String) {
-        SavedNodes.add(Node(value))
+        savedNodes.add(Node(value))
     }
 
     fun loadNode(fileName: String) {
-        loadFromFile(fileName)?.let { SavedNodes.add(it) }
+        loadFromFile(fileName)?.let { savedNodes.add(it) }
     }
 
-    fun saveNode(fileName: String, index: Int) {
-        if(index >= SavedNodes.size) {
-            throw IllegalArgumentException("Index out of range")
-        }
-        saveToFile(fileName, SavedNodes[index])
+    fun saveNode(index: Int, fileName: String) {
+        checkIndex(index)
+        saveToFile(fileName, savedNodes[index])
+    }
+
+    fun removeNode(index: Int) {
+        checkIndex(index)
+        savedNodes.removeAt(index)
+    }
+
+    fun showNode(index: Int): Node {
+        checkIndex(index)
+        return savedNodes[index]
+    }
+
+    fun changeValue(index: Int, newValue: String) {
+        checkIndex(index)
+        savedNodes[index].value = newValue
+    }
+
+    fun getSize(): Int = savedNodes.size
+
+    fun takeChild(nodeInd: Int, childInd: Int) {
+        checkIndex(nodeInd, childInd)
+        savedNodes.add(savedNodes[nodeInd].nodes[childInd])
+    }
+
+    fun addChild(nodeInd: Int, childInd: Int) {
+        checkIndex(nodeInd)
+        checkIndex(childInd)
+        savedNodes[nodeInd].nodes.add(savedNodes[childInd])
+    }
+
+    fun removeChild(nodeInd: Int, childInd: Int) {
+        checkIndex(nodeInd, childInd)
+        savedNodes[nodeInd].nodes.removeAt(childInd)
     }
 }
 
@@ -93,6 +137,7 @@ fun main() {
     val nodeOperator = NodeOperator()
     var line: String?
     while(true) {
+        print("> ")
         line = readLine()
         if(line == null) {
             break
@@ -102,7 +147,7 @@ fun main() {
             continue
         }
         when (args[0]) {
-            "add_isolated" -> {
+            "add" -> {
                 if(args.size < 2) {
                     println("Wrong number of arguments")
                     continue
@@ -121,7 +166,67 @@ fun main() {
                     println("Wrong number of arguments")
                     continue
                 }
-                nodeOperator.saveNode(args[1], args[2].toInt())
+                nodeOperator.saveNode(args[1].toInt(), args[2])
+            }
+            "rm" -> {
+                if(args.size < 2) {
+                    println("Wrong number of arguments")
+                    continue
+                }
+                nodeOperator.removeNode(args[1].toInt())
+            }
+            "show" -> {
+                if(args.size < 2) {
+                    println("Wrong number of arguments")
+                    continue
+                }
+                val ans = nodeOperator.showNode(args[1].toInt())
+                println(ans.value)
+                for (i in ans.nodes) {
+                    print(i.value + " ")
+                }
+                println()
+            }
+            "size" -> {
+                println(nodeOperator.getSize())
+            }
+            "list" -> {
+                for (i in 0 until nodeOperator.getSize()) {
+                    print(nodeOperator.showNode(args[1].toInt()).value + " ")
+                }
+                println()
+            }
+            "change" -> {
+                if(args.size < 3) {
+                    println("Wrong number of arguments")
+                    continue
+                }
+                nodeOperator.changeValue(args[1].toInt(), args[2])
+            }
+            "take_child" -> {
+                if(args.size < 3) {
+                    println("Wrong number of arguments")
+                    continue
+                }
+                nodeOperator.takeChild(args[1].toInt(), args[2].toInt())
+            }
+            "add_child" -> {
+                if(args.size < 3) {
+                    println("Wrong number of arguments")
+                    continue
+                }
+                nodeOperator.addChild(args[1].toInt(), args[2].toInt())
+            }
+            "rm_child" -> {
+                if(args.size < 3) {
+                    println("Wrong number of arguments")
+                    continue
+                }
+                nodeOperator.removeChild(args[1].toInt(), args[2].toInt())
+            }
+            else -> {
+                println("Unknown command " + args[0])
+                continue
             }
         }
     }
